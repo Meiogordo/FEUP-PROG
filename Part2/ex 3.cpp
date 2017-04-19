d55604d703c7ee59222f4a8ff96f0a89b9eda470
Meiogordo <gamer-go@hotmail.com>
2017-04-19 11:28:13 +0100

    Exercise 3 finished

--- Part2/ex 3.cpp
+++ Part2/ex 3.cpp
@@ -57,25 +57,63 @@ private:
 class Invoice {
 public:
 	Invoice(const Client &c, const vector<Item> &items) : c(c), items(items) {};
-	void printCompleteInvoice(ostream &out) const {
+	void print(ostream &out) const {
+		//Name and address
 		out << c.getName() << endl;
 		out << c.getFormattedAddress();
 		out << endl << endl;
+
+		//Table header
 		out << left << setw(20) << "Description" << right << setw(6) << "Price"
 			<< setw(5) << "Qty" << setw(8) << "Total";
 		out << endl;
-		out << string(20, '-') << " " << string(6, '-') << " " << string(5, '-') << " " << string(8, '-');
+		out << string(20, '-') << " " << string(6, '-') << " " << string(5, '-') << " " << string(8, '-') << endl;
 		
+		double totalAmountDue = 0;
+
+		//Printing each line of items
+		for (int i = 0; i < items.size(); i++) {
+			out << left << setw(20) << items[i].getDescription() << " ";
+			out << right << setw(6) << items[i].getPrice() << " ";
+			out << setw(5) << items[i].getQuantity() << " ";
+			out << setw(8) << items[i].getTotal() << endl;
+			//Calculating the total amount due by adding the item totals
+			totalAmountDue += items[i].getTotal();
+		}
+
+		out << endl;
+		out << "Amount due: " << totalAmountDue << " euro" << endl;
 	};
 private:
 	Client c;
 	vector<Item> items;
 };
 
+//Overloading << for the Invoice class (not necessary but used it to also learn how to do this)
+ostream& operator<< (std::ostream& os, const Invoice &i) {
+	i.print(os);
+	return os;
+}
+
 int main() {
 
-	Client c("Cliente 1", "Rua rua", -1, "4400-000", "Malgem Sul");
+	/*Client c("Cliente 1", "Rua rua", -1, "4400-000", "Malgem Sul");
+	cout << c.getName() << endl << c.getFormattedAddress() << endl;*/
 
-	cout << c.getName() << endl << c.getFormattedAddress() << endl;
+	//Iomanip to make output the same
+	cout << fixed;
+	cout.precision(2);
 
+	//Testing with the example data to check if it works the same
+	Client c1("DEI- FEUP", "Rua Dr. Roberto Frias", -1, "4200-465", "Porto");
+	Product p1("Computer", 999.90);
+	Item i1(p1, 10);
+	Product p2("Printer", 149.90);
+	Item i2(p2, 1);
+
+	vector<Item> items = { i1, i2 };
+
+	Invoice inv(c1, items);
+
+	cout << inv;
 }
